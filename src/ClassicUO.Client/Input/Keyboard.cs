e61664a8893f7602f4aeddda40010ab27c38f0e7
TazmanianTad <bittiez@gmail.com>
2025-12-22 07:18:02 -0700

    Potential drag select fix

--- src/ClassicUO.Client/Input/Keyboard.cs
+++ src/ClassicUO.Client/Input/Keyboard.cs
@@ -2,7 +2,7 @@
 
 // Copyright (c) 2021, andreakarasho
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 // 1. Redistributions of source code must retain the above copyright
@@ -16,7 +16,7 @@
 // 4. Neither the name of the copyright holder nor the
 //    names of its contributors may be used to endorse or promote products
 //    derived from this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -36,63 +36,40 @@ namespace ClassicUO.Input
 {
     internal static class Keyboard
     {
-        private static SDL.SDL_Keycode _code;
-
-
         public static SDL.SDL_Keymod IgnoreKeyMod { get; } = SDL.SDL_Keymod.KMOD_CAPS | SDL.SDL_Keymod.KMOD_NUM | SDL.SDL_Keymod.KMOD_MODE | SDL.SDL_Keymod.KMOD_RESERVED;
 
         public static bool Alt { get; private set; }
         public static bool Shift { get; private set; }
         public static bool Ctrl { get; private set; }
 
+        public static void OnKeyUp(SDL.SDL_KeyboardEvent e) => OnKeyEvent(e);
 
-        //public static bool IsKeyPressed(SDL.SDL_Keycode code)
-        //{
-        //    return code != SDL.SDL_Keycode.SDLK_UNKNOWN && _code == code;
-        //}
+        public static void OnKeyDown(SDL.SDL_KeyboardEvent e) => OnKeyEvent(e);
 
-        //public static bool IsModPressed(SDL.SDL_Keymod mod, SDL.SDL_Keymod tocheck)
-        //{
-        //    mod ^= mod & IgnoreKeyMod;
-
-        //    return tocheck == mod || mod != SDL.SDL_Keymod.KMOD_NONE && (mod & tocheck) != 0;
-        //}
-
-        public static void OnKeyUp(SDL.SDL_KeyboardEvent e)
+        private static void OnKeyEvent(SDL.SDL_KeyboardEvent e)
         {
-            SDL.SDL_Keymod mod = e.keysym.mod & ~IgnoreKeyMod;
-
-            if ((mod & (SDL.SDL_Keymod.KMOD_RALT | SDL.SDL_Keymod.KMOD_LCTRL)) == (SDL.SDL_Keymod.KMOD_RALT | SDL.SDL_Keymod.KMOD_LCTRL))
-            {
-                e.keysym.sym = SDL.SDL_Keycode.SDLK_UNKNOWN;
-                e.keysym.mod = SDL.SDL_Keymod.KMOD_NONE;
-            }
-
-            Shift = (e.keysym.mod & SDL.SDL_Keymod.KMOD_SHIFT) != SDL.SDL_Keymod.KMOD_NONE;
-            Alt = (e.keysym.mod & SDL.SDL_Keymod.KMOD_ALT) != SDL.SDL_Keymod.KMOD_NONE;
-            Ctrl = (e.keysym.mod & SDL.SDL_Keymod.KMOD_CTRL) != SDL.SDL_Keymod.KMOD_NONE;
-
-            _code = SDL.SDL_Keycode.SDLK_UNKNOWN;
+            UpdateModifiers(e.keysym.mod);
         }
 
-        public static void OnKeyDown(SDL.SDL_KeyboardEvent e)
+        private static void UpdateModifiers(SDL.SDL_Keymod e)
         {
-            SDL.SDL_Keymod mod = e.keysym.mod & ~IgnoreKeyMod;
+            SDL.SDL_Keymod mod = e & ~IgnoreKeyMod;
+            SDL.SDL_Keymod filtered = mod;
 
             if ((mod & (SDL.SDL_Keymod.KMOD_RALT | SDL.SDL_Keymod.KMOD_LCTRL)) == (SDL.SDL_Keymod.KMOD_RALT | SDL.SDL_Keymod.KMOD_LCTRL))
             {
-                e.keysym.sym = SDL.SDL_Keycode.SDLK_UNKNOWN;
-                e.keysym.mod = SDL.SDL_Keymod.KMOD_NONE;
+                filtered = SDL.SDL_Keymod.KMOD_NONE;
             }
 
-            Shift = (e.keysym.mod & SDL.SDL_Keymod.KMOD_SHIFT) != SDL.SDL_Keymod.KMOD_NONE;
-            Alt = (e.keysym.mod & SDL.SDL_Keymod.KMOD_ALT) != SDL.SDL_Keymod.KMOD_NONE;
-            Ctrl = (e.keysym.mod & SDL.SDL_Keymod.KMOD_CTRL) != SDL.SDL_Keymod.KMOD_NONE;
+            Shift = (filtered & SDL.SDL_Keymod.KMOD_SHIFT) != SDL.SDL_Keymod.KMOD_NONE;
+            Alt = (filtered & SDL.SDL_Keymod.KMOD_ALT) != SDL.SDL_Keymod.KMOD_NONE;
+            Ctrl = (filtered & SDL.SDL_Keymod.KMOD_CTRL) != SDL.SDL_Keymod.KMOD_NONE;
+        }
 
-            if (e.keysym.sym != SDL.SDL_Keycode.SDLK_UNKNOWN)
-            {
-                _code = e.keysym.sym;
-            }
+        public static void Refresh()
+        {
+            SDL.SDL_Keymod mod = SDL.SDL_GetModState();
+            UpdateModifiers(mod);
         }
     }
 }
